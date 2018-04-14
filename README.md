fdfbadc900c0dd3438dd37621c99bf2d837114c9
Andrew Ksendzoff <dacoconuttman@gmail.com>
2018-04-14 18:10:28 -0500

    Initial stuff

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # dacoconuttbot
+
+Twitch chat bot
+
+## Features
+- [ ] Spotify song requests
+- [ ] Song skips
+- [ ] Currency system
+- [ ] Easter eggs
+
+## Requirements
+- Twitch OAuth token
+- Spotify Application (see developer.spotify.com)
+
+## Usage
+1. Clone this repo
+2. Run ```npm install```
+3. Run ```npm start```
---
+++ conf.default.js
@@ -0,0 +1,14 @@
+module.exports = {
+  spotify: {
+    clientId: '<spotify client id>',
+    clientSecret: '<spotify client secret>',
+    redirectUri: 'http://example.com.callback'
+  },
+  twitch: {
+    port: 6667,
+    nick: '<twitch bot username>',
+    password: '<OAuth token>',
+    server: 'irc.twitch.tv',
+    channel: '#<channel name>'
+  }
+}
